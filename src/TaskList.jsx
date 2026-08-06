ea333ef08e51c909580a729699b810c32dae52a3
kurosu66 <kurosu.66@gmail.com>
2026-08-06 22:36:01 +0900

    compledの状態が見た目に反映されるようにする

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,6 +1,6 @@
 export default function TasksList({tasks}) {
   const taskList = tasks.map(task =>
-    <li key={task.id}>{task.title}</li>
+    <li key={task.id}>{task.completed ? task.title + '  ✅' : task.title }</li>
   );
   return (
     <ul>{taskList}</ul>
